828316602b4ee2e393d887c6249ce63bb63a5924
isilay-subasi <isil.subasi19@gmail.com>
2022-04-19 13:10:07 +0300

    timer updated

--- QuizAppOdev/app/src/main/java/com/isilsubasi/quizapp/activities/MainActivity.java
+++ QuizAppOdev/app/src/main/java/com/isilsubasi/quizapp/activities/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.CountDownTimer;
 
 import com.isilsubasi.quizapp.R;
 import com.isilsubasi.quizapp.activities.LoginActivity;
+import com.isilsubasi.quizapp.util.Constans;
 
 //Uygulama başladığında ekranda gözüken ilk aktivitedir.
 public class MainActivity extends AppCompatActivity {
@@ -17,43 +18,38 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //3 saniye sonra diğer ekrana geçmesi için oluşturulmuş fonksiyondur.
-        countDownTimerFunction(3000,1000);
+        SplashScreenTimer splashScreenTimer=new SplashScreenTimer(Constans.SPLASH_SCREEN_TIMER_MILIS,Constans.SPLASH_SCREEN_INVERTAL_MILIS);
+        splashScreenTimer.start();
+
 
 
     }
-
-
 
     /*
-    Aşağıdaki fonksiyon 3 saniyelik bir timer başlatır ve bittiğinde LoginActivity
-    ekranını açıp bu ekranı öldürür.
+    Aşağıdaki class CountDownTimer'dan kalıtım alır.
+    Ve nesne oluşturulduğu yerde nesnenin içerisine girilen parameteye bağlı olarak zamanlayıcı oluşturarak,
+    süre bitince LoginActivity.class açar.
      */
-    public void countDownTimerFunction(int milis,int interval){
-        //3000 milisaniye - 3 saniyeye denk gelir.
-        //interval - aralık
-        new CountDownTimer(milis, interval) {
 
-            //OnTick metodu geri sayım süresince yapılacak değişiklikler
-            @Override
-            public void onTick(long l) {
+    class SplashScreenTimer extends CountDownTimer{
 
-            }
+        public SplashScreenTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+        //Geri sayım süresince yapılacak değişiklikler
+        @Override
+        public void onTick(long l) {
 
-            //süre bittiğinde yapılacaklar
-            @Override
-            public void onFinish() {
-
-                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-                startActivity(intent);
-                finish();
-
-            }
-        }.start();
-
-
-
+        }
+        ///süre bittiğinde yapılacaklar
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
     }
+
 
 
 
--- QuizAppOdev/app/src/main/java/com/isilsubasi/quizapp/util/Constans.java
+++ QuizAppOdev/app/src/main/java/com/isilsubasi/quizapp/util/Constans.java
@@ -3,6 +3,7 @@ package com.isilsubasi.quizapp.util;
 public class Constans {
 
     public static final String PREF_OYUNCU_ADI_PARAMETRESİ="OYUNCU_ADİ";
-
+    public static final long SPLASH_SCREEN_TIMER_MILIS=3000;
+    public static final long SPLASH_SCREEN_INVERTAL_MILIS=1000;
 
 }
